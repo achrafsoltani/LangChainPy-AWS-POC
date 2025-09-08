1f65c1f97182cdab3a80491b24ab1a831a0a26d5
Achraf SOLTANI <achraf.soltani@gmail.com>
2025-09-08 12:52:38 +0100

    Adding QDrant Vectore Store (QDrant Cloud)

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 # Ensure your AWS credentials are configured
 
 from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage
+from langchain_aws import BedrockEmbeddings
+from qdrant_client.models import Distance, VectorParams
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
+import os
 
+# LLM Setup
 model = init_chat_model("eu.anthropic.claude-3-7-sonnet-20250219-v1:0", model_provider="bedrock_converse")
 
-from langchain_core.messages import HumanMessage
+# Embedding model setup
+embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v2:0")
+
+# QDrant Client
+qdrant_client = QdrantClient(
+    url=os.getenv("QDRANT_CLOUD_URL"),
+    api_key=os.getenv("QDRANT_CLOUD_KEY") ,
+)
+
 
 output = model.invoke([HumanMessage(content="Hi! I'm Bob")])
 print(output.content)
