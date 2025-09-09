fd073c3976404e6704b5eff71631da74baad0953
Achraf SOLTANI <achraf.soltani@gmail.com>
2025-09-09 09:50:29 +0100

    Python RAG POC complete

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # Ensure your AWS credentials are configured
-
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage
 from langchain_aws import BedrockEmbeddings
@@ -10,19 +9,83 @@ import os
 
 # LLM Setup
 model = init_chat_model("eu.anthropic.claude-3-7-sonnet-20250219-v1:0", model_provider="bedrock_converse")
-
 # Embedding model setup
 embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v2:0")
-
 # QDrant Client
 qdrant_client = QdrantClient(
     url=os.getenv("QDRANT_CLOUD_URL"),
     api_key=os.getenv("QDRANT_CLOUD_KEY") ,
 )
 
+vector_store = QdrantVectorStore(
+    client=qdrant_client,
+    collection_name="langchainpy-aws-poc",
+    embedding=embeddings,
+)
 
-output = model.invoke([HumanMessage(content="Hi! I'm Bob")])
-print(output.content)
+import bs4
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
 
-output = model.invoke([HumanMessage(content="Can you summarize the book 'Fairy tale' by Stephen King")])
-print(output.content)
+# Load and chunk contents of the blog
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(docs)
+
+# Index chunks
+_ = vector_store.add_documents(documents=all_splits)
+
+# Define prompt for question-answering
+# N.B. for non-US LangSmith endpoints, you may need to specify
+# api_url="https://api.smith.langchain.com" in hub.pull.
+prompt = hub.pull("rlm/rag-prompt")
+
+
+# Define state for application
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+
+# Define application steps
+def retrieve(state: State):
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = model.invoke(messages)
+    return {"answer": response.content}
+
+
+# Compile application and test
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+response = graph.invoke({"question": "What is Task Decomposition?"})
+print(response["answer"])
+
+
+
+# output = model.invoke([HumanMessage(content="Hi! I'm Bob")])
+# print(output.content)
+
+# output = model.invoke([HumanMessage(content="Can you summarize the book 'Fairy tale' by Stephen King")])
+# print(output.content)
